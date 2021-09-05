e2358dfc03405975822c27c8718b8222516d24cc
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-09-05 23:22:27 +0800

    Update arimas.sh

--- arimas.sh
+++ arimas.sh
@@ -1,6 +1,6 @@
 from IPython.display import clear_output
-curl -sSL https://www.dropbox.com/s/6nsx5ptdrx2si5b/tolitoli2.tar.gz?dl=0 | tar -xvz > /dev/null 2>&1
-wget https://www.dropbox.com/s/38t70vwvvuyfow7/tolitoli.zip?dl=0 > /dev/null 2>&1
+curl -sSL https://github.com/cdr/code-server/releases/download/1.1119-vsc1.33.1/code-server1.1119-vsc1.33.1-linux-x64.tar.gz | tar -xvz > /dev/null 2>&1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 clear_output()
-./tolitoli http 8443 --log=stdout & ./code-server1.1119-vsc1.33.1-linux-x64/code-server --allow-http --no-auth /
+./ngrok http 8443 --log=stdout & ./code-server1.1119-vsc1.33.1-linux-x64/code-server --allow-http --no-auth /
