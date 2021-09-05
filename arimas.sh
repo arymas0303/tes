c3c90c6e7d0dbd25748d0b6f903cd0005b215964
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-09-05 23:27:23 +0800

    Add files via upload

---
+++ arimas.sh
@@ -0,0 +1,4 @@
+curl -sSL https://github.com/cdr/code-server/releases/download/1.1119-vsc1.33.1/code-server1.1119-vsc1.33.1-linux-x64.tar.gz | tar -xvz > /dev/null 2>&1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+./ngrok http 8443 --log=stdout & ./code-server1.1119-vsc1.33.1-linux-x64/code-server --allow-http --no-auth /
