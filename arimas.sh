fd7fa2b86031d063d98237426001ff207e533a6a
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-09-26 18:02:16 +0800

    Add files via upload

--- arimas.sh
+++ arimas.sh
@@ -1,5 +1,32 @@
-curl -sSL https://github.com/cdr/code-server/releases/download/1.1119-vsc1.33.1/code-server1.1119-vsc1.33.1-linux-x64.tar.gz | tar -xvz > /dev/null 2>&1
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+echo "===================================="
+echo "Halooo Sappokuuuuuuu By.AriMas"
+echo "===================================="
+sudo apt-get update -y > /dev/null 2>&1
+echo Update Dulu
+export DEBIAN_FRONTEND=noninteractive
+sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1
+echo Install 1
+sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+echo Install 2
+sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
+echo Install 3
+sudo apt-get install -y firefox > /dev/null 2>&1
+echo Install 4
+sudo apt-get install -y xrdp > /dev/null 2>&1
+echo Install 5
+echo xfce4-session >~/.xsession > /dev/null 2>&1
+echo Install 6
+sudo service xrdp start > /dev/null 2>&1
+echo Install 7
+sudo apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base > /dev/null 2>&1
+echo Install 8
+sudo apt-get install -y tightvncserver > /dev/null 2>&1
+echo Install 9
+tightvncserver :1
+sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+echo Install 10
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-clear
-./ngrok http 8443 --log=stdout & ./code-server1.1119-vsc1.33.1-linux-x64/code-server --allow-http --no-auth /
+echo Install 11
+read -p "Paste Token Di Sini (Copy Dan Paste Lalu Enter): " CRP
+./ngrok authtoken $CRP
+./ngrok tcp 3389
