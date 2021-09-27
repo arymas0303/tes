b868177ad368174bd960b34d144a288f22fd1483
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-09-28 05:26:59 +0800

    Update arimas.sh

--- arimas.sh
+++ arimas.sh
@@ -2,14 +2,14 @@
 echo "===================================="
 echo "Halooo Sappokuuuuuuu By.AriMas"
 echo "===================================="
-sudo apt-get update -y
+sudo apt-get update -y > /dev/null 2>&1
 export DEBIAN_FRONTEND=noninteractive
-sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
-sudo apt remove --assume-yes gnome-terminal
-sudo apt install --assume-yes xscreensaver
-sudo apt-get install -y firefox
-sudo apt-get install -y xrdp
-sudo service xrdp start
+sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1
+sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
+sudo apt-get install -y firefox > /dev/null 2>&1
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
 sudo apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base > /dev/null 2>&1
-sudo apt-get install -y tightvncserver
+sudo apt-get install -y tightvncserver > /dev/null 2>&1
 tightvncserver :1
