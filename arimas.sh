a04ea03941d4ba5e7b356f4d5b8cf7847cfea714
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-10-02 22:05:47 +0800

    Update arimas.sh

--- arimas.sh
+++ arimas.sh
@@ -6,10 +6,6 @@ sudo apt-get update -y
 export DEBIAN_FRONTEND=noninteractive
 sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
 sudo apt remove --assume-yes gnome-terminal
-sudo apt install --assume-yes xscreensaver
-sudo apt-get install -y firefox
 sudo apt-get install -y xrdp
 sudo service xrdp start
 sudo apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
-sudo apt-get install -y tightvncserver
-tightvncserver :1
