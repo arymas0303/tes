6df2e39c4e22e1a610fe00b789f34ae78b8e4381
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-10-10 03:54:11 +0800

    Update arm.sh

--- arm.sh
+++ arm.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update && apt-get upgrade -y
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-cd ccminer
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
