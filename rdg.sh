55d05d66635c21c8c14b83a0a64e6d6c350d5154
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-10-11 01:40:31 +0800

    Update rdg.sh

--- rdg.sh
+++ rdg.sh
@@ -11,7 +11,4 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt remove --assume-yes gnome-terminal
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes  google-chrome-stable \
-    --with-source=google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
