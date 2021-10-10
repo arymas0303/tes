f7d887889124bb67821b121add5f61934e6a73b0
arymas0303 <38964495+arymas0303@users.noreply.github.com>
2021-10-11 01:45:35 +0800

    Update rdg.sh

--- rdg.sh
+++ rdg.sh
@@ -4,9 +4,8 @@ echo "Script By.AriMas"
 echo "===================================="
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt update
-sudo apt install --assume-yes \
-    --with-source=chrome-remote-desktop_current_amd64.deb \
-    chrome-remote-desktop
+sudo apt install --assume-yes
+sudo apt install --with-source=chrome-remote-desktop_current_amd64.deb
 export DEBIAN_FRONTEND=noninteractive
 sudo apt remove --assume-yes gnome-terminal
 sudo apt install --assume-yes xscreensaver
